39daa93b4a59a241b0fdb96ac15cd2c5f176b41d
wangz <wangz@8space.com.cn>
2014-11-14 17:52:22 +0800

    改横屏

--- src/print/PrintTest.java
+++ src/print/PrintTest.java
@@ -157,16 +157,21 @@ public class PrintTest implements Printable {
     public static void main(String[] args) {
         // 通俗理解就是书、文档
         Book book = new Book();
-        // 设置成竖打
         PageFormat pf = new PageFormat();
-        pf.setOrientation(PageFormat.PORTRAIT);
+        // 设置成横打
+        pf.setOrientation(PageFormat.REVERSE_LANDSCAPE);
+        // 设置成竖打
+        // pf.setOrientation(PageFormat.PORTRAIT);
         // 通过Paper设置页面的空白边距和可打印区域。必须与实际打印纸张大小相符。
         Paper p = new Paper();
         // 纸张大小
         p.setSize(590, 840);
         // double x, double y, double width, double height
         // p.setImageableArea(10, 10, 590, 840);
-        p.setImageableArea(72, 72, 590, 840);
+        // 横打这样子配置
+        p.setImageableArea(-72, 72, 590, 840);
+        // 竖着大这样子配置
+        // p.setImageableArea(72, 72, 590, 840);
         // A4(595 X 842)设置打印区域，其实0，0应该是72，72，因为A4纸的默认X,Y边距是72
         pf.setPaper(p);
         // 把 PageFormat 和 Printable 添加到书中，组成一个页面
@@ -186,5 +191,4 @@ public class PrintTest implements Printable {
             e.printStackTrace();
         }
     }
-
 }
