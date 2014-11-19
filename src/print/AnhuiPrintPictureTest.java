b58eeecb966fb8d9a8a086f1effebf0b4c1d5d64
wangz <wangz@8space.com.cn>
2014-11-19 16:55:18 +0800

    提交打印图片类

---
+++ src/print/AnhuiPrintPictureTest.java
@@ -0,0 +1,177 @@
+/*****************************************************************************
+ * 安徽食品药品监督管理局追溯系统
+ *****************************************************************************
+ * PrintPicture.java
+ *----------------------------------------------------------------------------
+ * (C) 沈阳八维时空科技发展
+ *     SHENYANG EIGHTSPACE TECHNOLOGY DEVELOPMENT CO.,LTD.  2014
+ *----------------------------------------------------------------------------
+ * Version    Date       By            Comment
+ * 1.0        2014-11-19    Poppy       新建
+ *****************************************************************************/
+package print;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.print.Book;
+import java.awt.print.PageFormat;
+import java.awt.print.Paper;
+import java.awt.print.Printable;
+import java.awt.print.PrinterException;
+import java.awt.print.PrinterJob;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+//import org.apache.commons.io.IOUtils;
+//import org.springframework.util.FileCopyUtils;
+
+/**
+ * 打印图片
+ * 
+ * @author Poppy
+ * @date 2014-11-19
+ * @since 1.0
+ */
+public class AnhuiPrintPictureTest implements Printable {
+
+    private Image src;
+
+    private Component c = null;
+
+    private int endX;
+
+    private int endY;
+
+
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.awt.print.Printable#print(java.awt.Graphics,
+     * java.awt.print.PageFormat, int)
+     */
+    @Override
+    public int print(Graphics graphics, PageFormat pageFormat, int pageIndex) throws PrinterException {
+        // 转换成Graphics2D
+        Graphics2D g2 = ( Graphics2D ) graphics;
+        // 设置打印颜色为黑色
+        g2.setColor(Color.black);
+        // 打印起点坐标
+        int startX = ( int ) pageFormat.getImageableX();
+        int startY = ( int ) pageFormat.getImageableY();
+        switch (pageIndex) {
+            case 0:
+                // 开始画图片的位置
+                g2.drawImage(src, startX, startY, endX, endY, c);
+                return PAGE_EXISTS;
+            default:
+                return NO_SUCH_PAGE;
+        }
+    }
+
+
+
+    /**
+     * 打印图片
+     * 
+     * @author Poppy
+     * @date 2014-11-19
+     * @param is
+     * @param fileId
+     * @throws IOException
+     * @since 1.0
+     */
+    public static void printPic(InputStream is, String fileId) throws IOException {
+        // byte[] bytes = IOUtils.toByteArray(is);这个项目没有报，暂时下面那么写
+        byte[] bytes = null;
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        baos.write(bytes);
+        // 获取绝对路径
+        // String basePath = play.Play.application().path().getAbsolutePath();
+        String basePath = "";// 这个项目没有play所以不能像上一行那么写，暂时这么代替
+        // 获取本地存储路径
+        String path = basePath + File.separator + "public" + File.separator + "tempPic";
+        // 将文件写到文件服务器硬盘
+        File file = new File(path);
+        // 判断文件是否存在
+        if (!file.exists()) {
+            file.mkdirs();
+        }
+        // 文件路径
+        String filePath = path + File.separator + fileId + ".jpg";
+        // 将bytes中的内容拷贝到file中
+        // FileCopyUtils.copy(baos.toByteArray(), new
+        // File(filePath));这个缺少包，所以注释掉，实际上不能少
+        AnhuiPrintPictureTest anhuiPrintPictureTest = new AnhuiPrintPictureTest();
+        // 打印图片，括号里写上本地图片的路径
+        anhuiPrintPictureTest.src = Toolkit.getDefaultToolkit().getImage(filePath);
+        // 设置图片的宽
+        int img_width = anhuiPrintPictureTest.src.getWidth(anhuiPrintPictureTest.c);
+        long ctm1 = System.currentTimeMillis();
+        // 如果宽是-1说明不知道宽是多少，意思就是说，这个时候图片还没有读取完，所以循环，一直到读取到了就跳出，如果一直读取不到，那么30秒后自动跳出。
+        while (img_width == -1) {
+            img_width = anhuiPrintPictureTest.src.getWidth(anhuiPrintPictureTest.c);
+            long ctm2 = System.currentTimeMillis();
+            if (ctm2 - ctm1 > 30000) {
+                break;
+            }
+        }
+        // 设置图片的高
+        int img_Height = anhuiPrintPictureTest.src.getHeight(anhuiPrintPictureTest.c);
+        long ctm3 = System.currentTimeMillis();
+        // 如果长是-1说明不知道宽是多少，意思就是说，这个时候图片还没有读取完，所以循环，一直到读取到了就跳出，如果一直读取不到，那么30秒后自动跳出。
+        while (img_width == -1) {
+            img_Height = anhuiPrintPictureTest.src.getHeight(anhuiPrintPictureTest.c);
+            long ctm4 = System.currentTimeMillis();
+            if (ctm4 - ctm3 > 30000) {
+                break;
+            }
+        }
+        System.out.println(img_Height + "," + img_width);
+        // 通俗理解就是书、文档
+        Book book = new Book();
+        // PageFomat，这个设置完了要放到book中的
+        PageFormat pf = new PageFormat();
+        // 通过Paper设置页面的空白边距和可打印区域。必须与实际打印纸张大小相符。
+        Paper p = new Paper();
+        // 纸张大小
+        p.setSize(590, 840);
+        // p.setImageableArea(10, 10, 590, 840);
+        // A4(595 X 842)设置打印区域，其实0，0应该是72，72，因为A4纸的默认X,Y边距是72
+        if (img_Height > img_width) {
+            // 高大于宽，竖着打
+            pf.setOrientation(PageFormat.PORTRAIT);
+            // 竖着大这样子配置
+            p.setImageableArea(72, 72, 446, 696);
+        } else {
+            // 宽大于高，横着打
+            pf.setOrientation(PageFormat.REVERSE_LANDSCAPE);
+            // 横打这样子配置
+            p.setImageableArea(72, 72, 446, 696);
+        }
+        anhuiPrintPictureTest.endX = img_width;
+        anhuiPrintPictureTest.endY = img_Height;
+        pf.setPaper(p);
+        // 把 PageFormat 和 Printable 添加到书中，组成一个页面
+        book.append(anhuiPrintPictureTest, pf);
+        // 获取打印服务对象
+        PrinterJob job = PrinterJob.getPrinterJob();
+        // 设置打印类
+        job.setPageable(book);
+        try {
+            // 可以用printDialog显示打印对话框，在用户确认后打印；也可以直接打印
+            boolean a = job.printDialog();
+            if (a) {
+                job.print();
+            }
+        } catch (PrinterException e) {
+            e.printStackTrace();
+        }
+    }
+}
