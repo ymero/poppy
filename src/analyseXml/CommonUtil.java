61f42ca9421263f13487aae140705cfa3875bc83
wangz <wangz@8space.com.cn>
2014-11-26 11:18:21 +0800

    去掉黄叹号

--- src/analyseXml/CommonUtil.java
+++ src/analyseXml/CommonUtil.java
@@ -4,7 +4,6 @@
 package analyseXml;
 
 import java.io.InputStream;
-import java.io.Writer;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -14,12 +13,6 @@ import javax.servlet.http.HttpServletRequest;
 import org.dom4j.Document;
 import org.dom4j.Element;
 import org.dom4j.io.SAXReader;
-
-import com.thoughtworks.xstream.XStream;
-import com.thoughtworks.xstream.core.util.QuickWriter;
-import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
-import com.thoughtworks.xstream.io.xml.PrettyPrintWriter;
-import com.thoughtworks.xstream.io.xml.XppDriver;
 
 /**
  * 消息工具类
