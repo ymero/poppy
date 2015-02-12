87040d1b91528a54c03c5a2390f7a11dc96b3ec1
wangz <wang-z@neunn.com>
2015-02-12 11:29:19 +0800

    akka commit~

--- src/akka/AkkaTest.java
+++ src/akka/AkkaTest.java
@@ -10,6 +10,8 @@ import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
 
+import com.typesafe.config.ConfigFactory;
+
 /**
  * akka测试类，将akka当jar包用的时候，需要导入三个jar包：akka-actor_2.10.jar，config.jar，scala-libray.jar。
  * 
@@ -21,7 +23,10 @@ public class AkkaTest {
 
     public static void main(String[] args) {
         System.out.println("start!");
-        ActorSystem system = ActorSystem.create("MySystem");
+        // ActorSystem system = ActorSystem.create("MySystem");
+        // 上一行注释掉的部分与以下代码的功能一致，只不以下是通过配置文件创建爱你system的。
+        ActorSystem system =
+                ActorSystem.create("WorldApplication", ConfigFactory.load().getConfig("akka"));
         // 创建hello实例
         ActorRef hello = system.actorOf(Props.create(HelloWorld.class), "hello");
         // 通过tell方法给hello发送消息
