47b2d6085558350913dfd42e639e57d54d73ef81
wangz <wang-z@neunn.com>
2015-02-12 16:47:25 +0800

    commit

--- src/akka/AkkaTest.java
+++ src/akka/AkkaTest.java
@@ -24,13 +24,13 @@ public class AkkaTest {
     public static void main(String[] args) {
         System.out.println("start!");
         // ActorSystem system = ActorSystem.create("MySystem");
-        // 上一行注释掉的部分与以下代码的功能一致，只不以下是通过配置文件创建爱你system的。
+        // 上一行注释掉的部分与以下代码的功能一致，只不以下是通过配置文件创建system的。
         ActorSystem system =
-                ActorSystem.create("WorldApplication", ConfigFactory.load().getConfig("akka"));
+                ActorSystem.create("akkaApplication", ConfigFactory.load().getConfig("akka"));
         // 创建hello实例
         ActorRef hello = system.actorOf(Props.create(HelloWorld.class), "hello");
+        //ActorRef remote = system.actorFor("akka.tcp://akkaApplication@127.0.0.1:2552/user/hello");
         // 通过tell方法给hello发送消息
         hello.tell(Greeter.Msg.GREET, hello);
     }
-
 }
